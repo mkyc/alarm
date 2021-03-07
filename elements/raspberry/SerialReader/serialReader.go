0c5287be0edc627f831aada7015a1cc7feed3b3d
Mateusz Kyc <mateusz.kyc@gmail.com>
2021-03-07 18:28:49 +0100

    add memory usage logging

--- elements/raspberry/SerialReader/serialReader.go
+++ elements/raspberry/SerialReader/serialReader.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"runtime"
 )
 import "log"
 import "github.com/tarm/serial"
@@ -22,8 +23,19 @@ func main() {
 	scanner := bufio.NewScanner(stream)
 	for scanner.Scan() {
 		log.Println(scanner.Text())
+		printMemUsage()
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func printMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	log.Printf("Alloc = %v KiB, TotalAlloc = %v KiB, Sys = %v KiB, NumGC = %v\n", bToKb(m.Alloc), bToKb(m.TotalAlloc), bToKb(m.Sys), m.NumGC)
+}
+
+func bToKb(b uint64) uint64 {
+	return b / 1024
 }
